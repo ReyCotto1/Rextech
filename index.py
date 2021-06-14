2efcae46a9ea835c0d3055ae0814eb16fe6bf7ff
Rey <rey.cotto1@upr.edu>
2021-06-14 18:25:33 -0400

    Initial Vercel

---
+++ index.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+env = "prod"
+
+
+@app.route('/')
+def about_me():
+    return render_template('about.html')
+
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+
+@app.route('/experiences')
+def work():
+    return render_template('experiences.html')
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/api')
+def api():
+    with open('data.json', mode='r') as my_file:
+        text = my_file.read()
+        return text
